eab636f2b4e3e1bb1ff4e54e056d80cffb1acda3
Lucas Peixoto <Lucaspeixotopro@gmail.com>
2025-12-01 09:47:22 -0300

    new functions using tokens from quest data

--- functions/main.py
+++ functions/main.py
@@ -4,7 +4,6 @@
 
 from firebase_functions import firestore_fn
 from firebase_functions.options import set_global_options
-from google.cloud import firestore
 from firebase_admin import initialize_app, messaging
 
 # For cost control, you can set the maximum number of containers that can be
@@ -23,45 +22,16 @@ initialize_app()
 
 @firestore_fn.on_document_created(document="campaigns/{campaignId}/quests/{questId}")
 def notify_new_quest(event: firestore_fn.Event[firestore_fn.DocumentSnapshot]) -> None:
-    db = firestore.Client()
     campaign_id = event.params['campaignId']
     quest_id = event.params['questId']
     
     print(f"New quest created in campaign {campaign_id} with ID {quest_id}")    
     
-
     quest_snapshot = event.data  # DocumentSnapshot
     quest_data = quest_snapshot.to_dict()
-    print("Quest data:", quest_data)
 
-    members_ref = db.collection("campaigns").document(campaign_id).collection("members")
-    members = members_ref.stream()
-
-
-    member_docs = []
-    fcm_tokens = []
-
-    for m in members:
-        data = m.to_dict()
-        member_docs.append(data)
-
-        user_id = data.get("userId")
-        user_doc = db.collection("users").document(user_id).get()
-
-        if user_doc.exists:
-            user_data = user_doc.to_dict()
-
-            # 3. Extract the token from user doc
-            token = user_data.get("fcmToken") or user_data.get("notificationToken")
-
-            if token:
-                fcm_tokens.append(token)
-        else:
-            print(f"User {user_id} not found in /users")
-
-
-    print("Members:", member_docs)
-    print("Tokens:", fcm_tokens)
+    fcm_tokens = quest_data.get('targetNotificationTokens', [])
+    print(f"NEW QUEST ({quest_id}) TOKENS: {fcm_tokens}")
 
     if fcm_tokens:
         title = f"{quest_data.get('title','Uma missão te espera!')}"
@@ -79,6 +49,13 @@ def notify_new_quest(event: firestore_fn.Event[firestore_fn.DocumentSnapshot]) -
             notification=messaging.Notification(
                 title=title,
                 body=body,
+            ),
+            apns=messaging.APNSConfig(
+                payload=messaging.APNSPayload(
+                    aps=messaging.Aps(
+                        sound="default"
+                    )
+                )
             ),
             tokens=fcm_tokens,
         )
